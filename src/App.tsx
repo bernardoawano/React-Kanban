05455c9a6ace20fb7b5a2f337b17176e547db30f
Bernardo Awano Muniz <bernardoawanomuniz@gmail.com>
2025-04-19 11:59:04 -0300

    project React-Kanban

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Box, Heading, Flex} from "@radix-ui/themes";
+import { CreateTaskForm } from "./components/CreateTaskForm";
+import { TaskBoard } from "./components/TaskBoard";
+import { TasksContextProvider } from "./contexts/TasksContext";
+
+export default function App() {
+  return (
+    <TasksContextProvider>
+      <Box maxWidth="80rem" mx="auto">
+        <Box height="4rem">
+          <Flex align="center" gap="4" height="100%">
+            <Heading as="h1" size="8" weight="light">React Kanban</Heading>
+            <CreateTaskForm />
+          </Flex>
+        </Box>
+
+        <Box>
+          <Heading as="h2"mb={"4"}>Quadro de Tarefas</Heading>
+          <TaskBoard />
+        </Box>
+      </Box>
+    </TasksContextProvider>
+  )
+}
